50a7c72625e26c28006750a4d44470a5e58bebb5
senemogluc <cagin.senemoglu@gmail.com>
2024-03-29 09:24:46 +0300

    initialized the project

---
+++ src/main/kotlin/com/senemogluc/emailsenderpoc/EmailSenderPocApplication.kt
@@ -0,0 +1,11 @@
+package com.senemogluc.emailsenderpoc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmailSenderPocApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmailSenderPocApplication>(*args)
+}
---
+++ src/test/kotlin/com/senemogluc/emailsenderpoc/EmailSenderPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.senemogluc.emailsenderpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmailSenderPocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
