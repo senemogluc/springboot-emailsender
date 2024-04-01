e01813c42ffff0e48239baf9952b3bb863e36c59
senemogluc <cagin.senemoglu@gmail.com>
2024-04-01 09:49:30 +0300

    added mail sender properties

--- src/main/kotlin/com/senemogluc/emailsenderpoc/config/AppConfig.kt
+++ src/main/kotlin/com/senemogluc/emailsenderpoc/config/AppConfig.kt
@@ -1,7 +1,10 @@
 package com.senemogluc.emailsenderpoc.config
 
+import com.senemogluc.emailsenderpoc.config.properties.MailSenderProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableConfigurationProperties(MailSenderProperties::class)
 class AppConfig {
 }
