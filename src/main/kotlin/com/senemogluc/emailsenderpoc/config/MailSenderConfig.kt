90ffe28e172ccfaf3d5a822277de95db0f2a7568
senemogluc <cagin.senemoglu@gmail.com>
2024-04-01 09:49:15 +0300

    added manual config and spring profiles

---
+++ src/main/kotlin/com/senemogluc/emailsenderpoc/config/MailSenderConfig.kt
@@ -0,0 +1,31 @@
+package com.senemogluc.emailsenderpoc.config
+
+import com.senemogluc.emailsenderpoc.config.properties.MailSenderProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
+import java.util.Properties
+
+@Profile("manual-config")
+@Configuration
+class MailSenderConfig(private val mailSenderProperties: MailSenderProperties) {
+
+    @Bean
+    fun javaMailSender(): JavaMailSender {
+        val mailSender = JavaMailSenderImpl()
+        mailSender.host = mailSenderProperties.host
+        mailSender.port = mailSenderProperties.port
+        mailSender.username = mailSenderProperties.username
+        mailSender.password = mailSenderProperties.password
+
+        configureJavaMailProperties(mailSender.javaMailProperties)
+        return mailSender
+    }
+
+    private fun configureJavaMailProperties(properties: Properties){
+        properties["mail.smtp.auth"] = mailSenderProperties.auth
+        properties["mail.smtp.starttls.enable"] = mailSenderProperties.starttlsEnable
+    }
+}
